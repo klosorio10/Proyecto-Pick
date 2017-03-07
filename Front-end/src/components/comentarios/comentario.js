c93148fd14bb1e90760167f8968f5365a0cce677
David Delgado <ddelgado2013@gmail.com>
2017-03-07 02:28:25 -0500

    Componente sin integración

--- Front-end/src/components/comentarios/comentario.js
+++ Front-end/src/components/comentarios/comentario.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-class Comentario extends Component{
+class Comentario extends Component{ // este es  el componente que hizo Fabio la vez pasada, y no lo integran al app.js o a la logica general de uso
   render(){
     return(
       <div>{this.props.text}</div>
