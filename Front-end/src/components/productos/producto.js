ab8de040b6fb614c2255aa568b7c1a69550b2d5d
David Delgado <ddelgado2013@gmail.com>
2017-03-07 02:18:49 -0500

    No se usa este componente

--- Front-end/src/components/productos/producto.js
+++ Front-end/src/components/productos/producto.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import Features from './features';
 
-class Producto extends Component {
-
+class Producto extends Component { // no usan este componente. Para qué es?
   constructor(props){
     super(props);
 
     this.state={
-      titulo:'Lavadora LG',
+      titulo:'Lavadora LG', // este estado deberia ser "genérico", pues dudo que haya sólo un producto.
       marca:'LG',
       imagen:'',
       caracteristicas:[]
